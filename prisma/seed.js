db921260c3995d9f243859c93cb161dd407405ad
jacobhogsten <hogsten1996@gmail.com>
2023-10-11 09:28:36 -0400

    schema seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,68 +1,72 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
 async function seed() {
   // Upserting Tags
   const easyTag = await prisma.tag.upsert({
-    where: { name: 'easy' },
+    where: { name: "easy" },
     update: {},
-    create: { name: 'easy' },
+    create: { name: "easy" },
   });
 
   const mexicanTag = await prisma.tag.upsert({
-    where: { name: 'mexican' },
+    where: { name: "mexican" },
     update: {},
-    create: { name: 'mexican' },
+    create: { name: "mexican" },
   });
 
   const southAmericaTag = await prisma.tag.upsert({
-    where: { name: 'south america' },
+    where: { name: "south america" },
     update: {},
-    create: { name: 'south america' },
+    create: { name: "south america" },
   });
 
   const beefTag = await prisma.tag.upsert({
-    where: { name: 'beef' },
+    where: { name: "beef" },
     update: {},
-    create: { name: 'beef' },
+    create: { name: "beef" },
   });
 
   // Upserting Ingredients
   const beefIngredient = await prisma.ingredient.upsert({
-    where: { name: 'beef' },
+    where: { name: "beef" },
     update: {},
-    create: { name: 'beef' },
+    create: { name: "beef" },
   });
 
   const tortillaIngredient = await prisma.ingredient.upsert({
-    where: { name: 'tortilla' },
+    where: { name: "tortilla" },
     update: {},
-    create: { name: 'tortilla' },
+    create: { name: "tortilla" },
   });
 
   const cheeseIngredient = await prisma.ingredient.upsert({
-    where: { name: 'cheese' },
+    where: { name: "cheese" },
     update: {},
-    create: { name: 'cheese' },
+    create: { name: "cheese" },
   });
 
   const salsaIngredient = await prisma.ingredient.upsert({
-    where: { name: 'salsa' },
+    where: { name: "salsa" },
     update: {},
-    create: { name: 'salsa' },
+    create: { name: "salsa" },
   });
 
   // Creating Recipe
   const recipe = await prisma.recipe.create({
     data: {
-      name: '(Test/Dev) Easy Beef Quesadilla',
-      details: 'A simple and delicious beef quesadilla.',
-      desc: 'Tasty beef quesadilla made with fresh ingredients.',
-      instructions: '1. Cook the beef until browned...\n2. Place the beef and cheese between two tortillas...\n3. Cook on a skillet until golden brown.\n4. Serve with salsa.',
-      imageUrl: 'https://girlheartfood.com/wp-content/uploads/2020/06/Ground-Beef-Quesadillas-4.jpg',
-      image2Url: 'https://img.taste.com.au/Fa2mth9d/taste/2016/11/beef-quesadillas-with-avocado-smash-69253-1.jpeg',
-      image3Url: 'https://mommygonehealthy.com/wp-content/uploads/2018/09/IMG_9033.jpg',
+      name: "(Test/Dev) Easy Beef Quesadilla",
+      details: "A simple and delicious beef quesadilla.",
+      desc: "Tasty beef quesadilla made with fresh ingredients.",
+      instructions:
+        "1. Cook the beef until browned...\n2. Place the beef and cheese between two tortillas...\n3. Cook on a skillet until golden brown.\n4. Serve with salsa.",
+      imageUrl:
+        "https://girlheartfood.com/wp-content/uploads/2020/06/Ground-Beef-Quesadillas-4.jpg",
+      image2Url:
+        "https://img.taste.com.au/Fa2mth9d/taste/2016/11/beef-quesadillas-with-avocado-smash-69253-1.jpeg",
+      image3Url:
+        "https://mommygonehealthy.com/wp-content/uploads/2018/09/IMG_9033.jpg",
       tagId: easyTag.id,
       ingredientId: beefIngredient.id,
     },
@@ -71,10 +75,26 @@ async function seed() {
   // Associating Ingredients with Recipe and their measurements
   await prisma.ingredient_recipe.createMany({
     data: [
-      { ingredientId: beefIngredient.id, recipeId: recipe.id, measurement: '1 lb' },
-      { ingredientId: tortillaIngredient.id, recipeId: recipe.id, measurement: '4 pieces' },
-      { ingredientId: cheeseIngredient.id, recipeId: recipe.id, measurement: '1 cup' },
-      { ingredientId: salsaIngredient.id, recipeId: recipe.id, measurement: '1/4 cup' },
+      {
+        ingredientId: beefIngredient.id,
+        recipeId: recipe.id,
+        measurement: "1 lb",
+      },
+      {
+        ingredientId: tortillaIngredient.id,
+        recipeId: recipe.id,
+        measurement: "4 pieces",
+      },
+      {
+        ingredientId: cheeseIngredient.id,
+        recipeId: recipe.id,
+        measurement: "1 cup",
+      },
+      {
+        ingredientId: salsaIngredient.id,
+        recipeId: recipe.id,
+        measurement: "1/4 cup",
+      },
     ],
   });
 }
@@ -87,5 +107,3 @@ seed()
   .finally(async () => {
     await prisma.$disconnect();
   });
-
-
